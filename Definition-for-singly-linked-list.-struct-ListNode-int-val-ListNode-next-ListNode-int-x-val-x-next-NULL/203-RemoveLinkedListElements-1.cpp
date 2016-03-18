8e090bdf3c8c7072120f3c0e7048da6c9cdc8e8a
winterfly <winterflyfei@gmail.com>
2016-03-18 13:46:06 -0400

    O(n) time, O(1) space, 36ms

---
+++ Definition-for-singly-linked-list.-struct-ListNode-int-val-ListNode-next-ListNode-int-x-val-x-next-NULL/203-RemoveLinkedListElements-1.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* dummy0=dummy;
+        while (dummy->next) {
+            if (dummy->next->val==val) {
+                dummy->next=dummy->next->next;
+            } else {
+                dummy=dummy->next;
+            }
+        }
+        return dummy0->next;
+        
+    }
+};
