81b0cfb00df03a3e9f62f8ca84811465da707ba3
winterfly <winterflyfei@gmail.com>
2016-03-18 16:30:31 -0400

    O(nlog(n)) time, O(1) space, 20ms
    Be careful about the while condition, how to set left and right,  and what to return.

---
+++ 287-FindTheDuplicateNumber-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int left=1,right=n-1;
+        while (left<=right){
+            int mid=left+(right-left)/2;
+            int count=0;
+            for (int i=0;i<n;i++) {
+                if (nums[i]<=mid) count++;
+            }
+            if (count<=mid) {
+                left=mid+1;
+            } else {
+                right=mid-1;
+            }
+        }
+        return left;
+    }
+};
