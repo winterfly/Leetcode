256bd43b26f2ee564fb946d1c162c65bf3c67a57
winterfly <winterflyfei@gmail.com>
2016-03-19 15:20:41 -0400

    O(n^2) time, O(1) space, 80ms
    This version should save some time than the first version although the runtime is still 80ms on passing 21 test cases. This is because when the current node is already bigger than all the previous ones, we do not need to do the insertion operation. 
    
    Note that if we did not allocate a new node for dummy (write ListNode* dummy; instead of ListNode* dummy=new ListNode(INT_MIN);), we would not pass the test cases.
    
    Also note that if we do not use the node pointer current but directly use pre->next, the runtime would be 94ms.
    
    Also note that if we write num=current->val and use num instead of current->val in the while condition, the runtime becomes 92ms.

--- 147-InsertionSortList-2.cpp
+++ 147-InsertionSortList-2.cpp
@@ -10,12 +10,12 @@ class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
         if (!head||!head->next) return head;
-        ListNode* dummy=new ListNode(INT_MIN);
+        ListNode* dummy=new ListNode(INT_MIN); // must allocate, otherwise runtime error
         dummy->next=head;
         ListNode* pre=head;
         while (pre->next) {
             ListNode* insert=dummy;
-            ListNode* current=pre->next;
+            ListNode* current=pre->next; // save time to use current other than directly using pre->next
             while (insert!=pre&&insert->next->val<=current->val) insert=insert->next;
             if (insert!=pre) {
                 ListNode* next=current->next;
