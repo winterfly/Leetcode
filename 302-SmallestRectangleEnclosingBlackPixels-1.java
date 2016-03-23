a25ff8d49ec46fbbe328ee3d8de1d81c4308730e
winterfly <winterflyfei@gmail.com>
2016-03-23 14:29:27 -0400

    O(mlog(n)+nlog(m)) time, O(1) space, 1ms
    From row 0 to row x, binary search to get the first row that contains the black pixel.
    From row x to row nr, binary search to get he first row that does not contain the black pixel.
    Do the same thing to the columns.

---
+++ 302-SmallestRectangleEnclosingBlackPixels-1.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int minArea(char[][] image, int x, int y) {
+        int nr=image.length;
+        if (nr==0) return 0;
+        int nc=image[0].length;
+        if (nc==0) return 0;
+        int left=search(image,0,x,0,nc-1,true,true);
+        int right=search(image,x+1,nr,0,nc-1,true,false);
+        int top=search(image,0,y,left,right,false,true);
+        int bottom=search(image,y+1,nc,left,right,false,false);
+        return (right-left)*(bottom-top);
+    }
+    
+    private int search(char[][] image, int low, int high, int min, int max, boolean horizontal, boolean black) {
+        while (low<high) {
+            int mid=low+(high-low)/2;
+            boolean containBlack=false;
+            for (int i=min;i<max;i++) {
+                if ((horizontal?image[i][mid]:image[mid][i])=='1') {
+                    containBlack=true;
+                    break;
+                }
+            }
+            if (containBlack==black) {
+                high=mid;
+            } else {
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+}
