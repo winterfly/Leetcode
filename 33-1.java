026397b3c46e49d0aff423c771f4370b381fc9f4
winterfly <winterflyfei@gmail.com>
2016-04-01 22:08:14 -0400

    O(log(n)) time, O(1) space, 1ms
    if (nums[left]<=nums[mid]) can not be if (nums[left]<nums[mid])!

---
+++ 33-1.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        int left=0,right=n-1,mid;
+        while (left<=right) {
+            mid=left+(right-left)/2;
+            if (nums[mid]==target) return mid;
+            if (nums[left]<=nums[mid]) {
+                if (nums[left]<=target&&target<nums[mid]) {
+                    right=mid-1;
+                } else {
+                    left=mid+1;
+                }
+            } else {
+                if (nums[mid]<target&&target<=nums[right]) {
+                    left=mid+1;
+                } else {
+                    right=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
