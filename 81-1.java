4fcf8016584fa1416f5767ab32407e19f0c81eb2
winterfly <winterflyfei@gmail.com>
2016-04-01 22:23:48 -0400

    O(log(n)) average O(n) (worst), O(1) space, 1ms
    Note the condition left != mid

---
+++ 81-1.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0,right = n-1,mid;
+        while (left <= right) {
+            mid = left + (right - left)/2;
+            if (nums[mid] == target) return true;
+            
+            if (left!=mid && nums[left]==nums[mid]) left++;
+            else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target&&target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1; 
+                }
+            }
+        }
+        return false;
+    }
+}
