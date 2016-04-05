f323853725502f13ba64ce2d182badc91ef2002c
winterfly <winterflyfei@gmail.com>
2016-04-05 12:04:37 -0400

    O(n) time, O(n) space (worst), 2ms
    O(log(n) if balanced

---
+++ 110-1.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return (height(root) != -1);
+    }
+    
+    private int height(TreeNode root) {
+        if (root == null) return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+        if (left == -1 || right == -1) return -1;
+        if (Math.abs(right - left) > 1) return -1;
+        return Math.max(left, right) + 1;
+    }
+}
