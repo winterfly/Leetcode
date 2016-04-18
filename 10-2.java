c8979fb1a4ed30916b5bb17affe330e62a8f1bae
winterfly <winterflyfei@gmail.com>
2016-04-18 00:09:49 -0400

    O(nm) time, O(nm) space, 7ms
    Code is changed when considering p.charAt.(j)='*'.

---
+++ 10-2.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        int n1 = s.length();
+        int n2 = p.length();
+        boolean[][] dp = new boolean[n1+1][n2+1];
+        
+        dp[0][0] = true;
+        for (int i = 0; i < n1; i++) {
+            dp[i+1][0] = false;
+        }
+        for (int j = 0; j < n2; j++) {
+            dp[0][j+1] = p.charAt(j) == '*' ? dp[0][j-1] : false;
+        }
+        
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                if (p.charAt(j) == '*') {
+                    if (dp[i+1][j-1]) {
+                        dp[i+1][j+1] = true;
+                    } else if (p.charAt(j-1) == '.' || p.charAt(j-1) == s.charAt(i)) {
+                        dp[i+1][j+1] = dp[i][j+1];
+                    }
+                } else if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {
+                    dp[i+1][j+1] = dp[i][j];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+}
