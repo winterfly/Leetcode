c7739098e548e85616b60343e6ad8d38e2bec77b
winterfly <winterflyfei@gmail.com>
2016-04-22 20:09:54 -0400

    O(nV) time, O(V) space, 22ms

---
+++ 322.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for (int i = 0; i <= amount; i++) {
+            if (dp[i] == Integer.MAX_VALUE) {
+                continue;
+            }
+            for (int j = 0; j < coins.length; j++) {
+                if (i + coins[j] <= amount ) {
+                    dp[i+coins[j]] = Math.min(dp[i+coins[j]], dp[i] + 1);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
